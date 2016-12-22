217bd458da6f309abfd9fc51a4df64164f49190b
Plavusa <masadordevic@gmail.com>
2016-12-22 21:42:21 +0100

    radium CardSymbol

--- makao-react/src/Card/CardSymbol.js
+++ makao-react/src/Card/CardSymbol.js
@@ -1,47 +1,47 @@
 import React from 'react';
+import Radium from 'radium';
 import {blackColor, redColor, isBlack} from './Card'
 
 class CardSymbol extends React.Component {
-    render() {
-
+    getStyles() {
         const circleSize = this.props.containerSize;
         const circleSignSize = circleSize * 0.7;
-        const headerCircleSignStyle = {
+        var style = {
             lineHeight: circleSize + "px",
             fontSize: circleSignSize + "px",
             margin: "auto",
             fontFamily: "monospace",
             fontWeight: "600",
             color: isBlack(this.props.symbol) ? blackColor : redColor,
+            ':hover': {
+                backgroundColor: "red",
+            }
         };
         if (this.props.lineHeight) {
-            headerCircleSignStyle.lineHeight = this.props.lineHeight + "px";
+            style.lineHeight = this.props.lineHeight + "px";
         }
         if (this.props.margin) {
-            headerCircleSignStyle.marginRight = this.props.margin + "px";
-            headerCircleSignStyle.marginLeft = this.props.margin + "px";
+            style.marginRight = this.props.margin + "px";
+            style.marginLeft = this.props.margin + "px";
         }
-        var cardSymbol;
+
+        return style;
+    }
+
+    render() {
+        let charCode;
         switch (this.props.symbol) {
-            case "spades":
-                cardSymbol = <span style={headerCircleSignStyle}>&spades;</span>;
-                break;
-            case "diamonds":
-                cardSymbol = <span style={headerCircleSignStyle}>&diams;</span>;
-                break;
-            case "clubs":
-                cardSymbol = <span style={headerCircleSignStyle}>&clubs;</span>;
-                break;
-            case "hearts":
-                cardSymbol = <span style={headerCircleSignStyle}>&hearts;</span>;
-                break;
+            case "spades": charCode = 9824; break;
+            case "diamonds": charCode = 9830; break;
+            case "clubs": charCode = 9827; break;
+            case "hearts": charCode = 9829; break;
             default:
         }
 
         return (
-            cardSymbol
+            <span style={this.getStyles()}>{String.fromCharCode(charCode)}</span>
         );
     }
 }
 
-export default CardSymbol;
+export default Radium(CardSymbol);
