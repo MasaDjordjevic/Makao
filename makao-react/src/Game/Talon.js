d3a1eb5db0933ad61f801ac36736141da7fa950b
Plavusa <masadordevic@gmail.com>
2016-12-25 17:27:57 +0100

    talon animation

--- makao-react/src/Game/Talon.js
+++ makao-react/src/Game/Talon.js
@@ -1,18 +1,40 @@
 /**
  * Created by Masa on 23-Dec-16.
  */
-import React from 'react'
+import React from 'react';
 import CardComponent from '../Card/CardComponent';
+import Transition from 'react-motion-ui-pack'
+
 
 class Talon extends React.Component {
+    getKey() {
+        if (!this.props.card)
+            return "back";
+        const card = this.props.card;
+        return card.number + card.symbol
+    }
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                {this.props.card ?
-                    <CardComponent cardHeight={this.props.cardHeight} card={this.props.card}/>
-                    : <CardComponent cardHeight={this.props.cardHeight} back/>
-                }
+                <Transition
+                    component={false}
+                    measure={false}
+                    enter={{
+                        scale: 1,
+                    }}
+                    leave={{
+                        scale: 1.2,
+                    }}
+                >
+                    <div key={this.getKey()}>
+                        {this.props.card ?
+                            <CardComponent cardHeight={this.props.cardHeight}
+                                           card={this.props.card}/>
+                            : <CardComponent cardHeight={this.props.cardHeight} back/>
+                        }
+                    </div>
+                </Transition>
             </div>
         );
     }
