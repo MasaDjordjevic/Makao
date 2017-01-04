64322a7309532fe2c66511114ea07f4370a14e9b
Plavusa <masadordevic@gmail.com>
2017-01-04 02:33:02 +0100

    scroll fail

--- makao-react/src/Game/ChatWrapper.js
+++ makao-react/src/Game/ChatWrapper.js
@@ -3,9 +3,23 @@
  */
 
 import React from 'react';
+import ReactDOM from 'react-dom'
+
 import Chat from './Chat';
 
 export default class ChatWrapper extends React.Component{
+    componentWillUpdate(){
+        const node = ReactDOM.findDOMNode(this);
+        this.scrollHeight = node.scrollHeight;
+        this.scrollTop = node.scrollTop;
+    }
+
+    componentDidUpdate() {
+        const node = ReactDOM.findDOMNode(this);
+        node.scrollTop = this.scrollTop + (node.scrollHeight - this.scrollHeight);
+        ReactDOM.findDOMNode(this).scrollTop = 0;
+
+    }
     render(){
         return(
             <div style={{
@@ -14,7 +28,7 @@ export default class ChatWrapper extends React.Component{
                 justifyContent: 'flex-end',
                 flexDirection: 'column'}}>
                 <Chat userId={this.props.userId} />
-                 </div>
+             </div>
         );
     }
 }
