52b9805ee58413b047ce08415807a4d966dce109
Plavusa <masadordevic@gmail.com>
2017-01-08 12:23:07 +0100

    next button

--- makao-react/src/Game/UserInfo.js
+++ makao-react/src/Game/UserInfo.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import GlobalValues from '../Gameplay/GlobalVariables';
 import TimerProgress from './TimerProgress';
-import {blueGrey300} from 'material-ui/styles/colors'
+import {blueGrey300} from 'material-ui/styles/colors';
+import FlatButton from 'material-ui/FlatButton';
+import FastForward from 'material-ui/svg-icons/av/fast-forward';
 
 class UserInfo extends React.Component {
     get styles() {
+        const nextIconSize = 30;
         return {
             container: {
                 display: 'flex',
-                flexDirection: 'column'
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'flex-start',
             },
             userName: {
                 color: blueGrey300,
@@ -20,6 +25,11 @@ class UserInfo extends React.Component {
                 marginTop: '5%',
                 width: '100%',
             },
+            nextButton:{
+                alignSelf: 'center',
+                width: '100%',
+                margin: '1% 0',
+            }
         }
     }
 
@@ -27,9 +37,21 @@ class UserInfo extends React.Component {
         alert("isteklo ti je vreme");
     }
 
+    handleNext(){
+        alert("sledeci");
+    }
+
     render() {
         return (
             <div style={{...this.styles.container, ...this.props.style}}>
+                <FlatButton
+                    label="next"
+                    labelPosition="before"
+                    primary={true}
+                    onClick={this.handleNext}
+                    style={this.styles.nextButton}
+                    icon={<FastForward />}
+                />
                 {this.props.myMove &&
                 <TimerProgress length={GlobalValues.handLength}
                                style={this.styles.timer}
