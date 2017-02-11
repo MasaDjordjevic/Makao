525a13374fc4fea8ef1c01e80f3e8f5ee9d53cf5
Plavusa <masadordevic@gmail.com>
2017-02-11 21:21:46 +0100

    join game

--- app/client/src/components/Lobby/JoinGame.js
+++ app/client/src/components/Lobby/JoinGame.js
@@ -1,6 +1,63 @@
 import React from 'react';
+import {Table, TableBody, TableRow, TableRowColumn}  from 'material-ui/Table';
+import { browserHistory } from 'react-router';
 
 class JoinGame extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            games: [
+                {
+                    id: 1,
+                    creator: 'Darko',
+                    current: 3,
+                    rules: {
+                        gameLimit: 300,
+                        timeLimit: 10,
+                        playerNumberMin: 3,
+                        playerNumberMax: 8,
+                        deckNumber: 2,
+                        rankFilter: 2
+                    }
+                },
+                {
+                    id: 2,
+                    creator: 'Marko',
+                    current: 2,
+                    rules: {
+                        gameLimit: 200,
+                        timeLimit: 15,
+                        playerNumberMin: 5,
+                        playerNumberMax: 5,
+                        deckNumber: 2,
+                        rankFilter: 2
+                    }
+                },
+                {
+                    id: 3,
+                    creator: 'Nikola',
+                    current: 3,
+                    rules: {
+                        gameLimit: 150,
+                        timeLimit: 30,
+                        playerNumberMin: 2,
+                        playerNumberMax: 6,
+                        deckNumber: 1,
+                        rankFilter: 15
+                    }
+                },
+            ]
+        };
+
+        this.joinGame = this.joinGame.bind(this);
+    }
+
+    joinGame(selectedRow){
+        if(!this.state.games[selectedRow]) return;
+        browserHistory.push('/Lobby:' + this.state.games[selectedRow].id);
+    }
+
     get styles() {
         return {
             container: {}
@@ -10,7 +67,25 @@ class JoinGame extends React.Component {
     render() {
         return (
             <div style={{...this.styles.container, ...this.props.style}}>
-                Join
+                <Table height='500px'
+                       fixedFooter={false}
+                       fixedHeader={false}
+                       selectable={true}
+                       onRowSelection={this.joinGame}>
+                    <TableBody displayRowCheckbox={false}>
+                        {
+                            this.state.games.map((game, i) =>
+                                <TableRow key={game.id} >
+                                    <TableRowColumn>{game.creator}</TableRowColumn>
+                                    <TableRowColumn>{game.rules.playerNumberMin}</TableRowColumn>
+                                    <TableRowColumn>{game.rules.playerNumberMax}</TableRowColumn>
+                                    <TableRowColumn>{game.current}</TableRowColumn>
+                                    <TableRowColumn><span>rules</span></TableRowColumn>
+                                </TableRow>
+                            )
+                        }
+                    </TableBody>
+                </Table>
             </div>
         );
     }
