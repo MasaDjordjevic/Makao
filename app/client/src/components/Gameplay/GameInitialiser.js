30384b20ac94fad58040a488a46a90282113c24e
Plavusa <masadordevic@gmail.com>
2017-02-11 23:19:33 +0100

    lobby WIP

--- app/client/src/components/Gameplay/GameInitialiser.js
+++ app/client/src/components/Gameplay/GameInitialiser.js
@@ -4,7 +4,7 @@ import FriendPicker from '../Lobby/FriendPicker';
 import Lobby from '../Lobby/Lobby';
 
 class GameInitialiser extends React.Component {
-    constructor(){
+    constructor() {
         super();
 
         this.state = {
@@ -14,21 +14,40 @@ class GameInitialiser extends React.Component {
         this.handleFriendInvite = this.handleFriendInvite.bind(this);
     }
 
-    handleFriendInvite(userId){
+    handleFriendInvite(userId) {
         console.log("invite friend: " + userId);
     }
 
     get styles() {
         return {
-            container: {}
+            container: {
+                height: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-around',
+            },
+            playersContainer: {
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+            },
+            friendPicker: {
+                minWidth: 450,
+                backgroundColor: 'white',
+                boxShadow: 'rgba(0, 0, 0, 0.117647) 0px 1px 6px, rgba(0, 0, 0, 0.117647) 0px 1px 4px',
+            }
         }
     }
 
     render() {
         return (
             <div style={{...this.styles.container, ...this.props.style}}>
-                <FriendPicker onPick={this.handleFriendInvite}/>
-                <RaisedButton onClick={this.props.onGameStart} label="Start game" />
+                <div style={this.styles.playersContainer}>
+                    <FriendPicker onPick={this.handleFriendInvite}
+                                  style={this.styles.friendPicker}/>
+                    <Lobby style={this.styles.friendPicker}/>
+                </div>
+                <RaisedButton onClick={this.props.onGameStart} label="Start game"/>
             </div>
         );
     }
--- app/client/src/components/Lobby/Lobby.js
+++ app/client/src/components/Lobby/Lobby.js
@@ -1,15 +1,42 @@
 import React from 'react';
-import CreateGame from './CreateGame';
-// import JoinGame from './JoinGame';
+
+import List from 'material-ui/List/List';
+import ListItem from 'material-ui/List/ListItem';
+import HourglassIcon from 'material-ui/svg-icons/action/hourglass-empty';
+import DoneIcon from 'material-ui/svg-icons/action/done';
 
 class Lobby extends React.Component {
+    constructor() {
+        super();
 
+        this.state = {
+            users: [
+                {
+                    id: 7825,
+                    firstName: 'Kristina',
+                    lastName: 'Stefanovic',
+                    ready: true,
+                },
+                {
+                    id: 1578,
+                    firstName: 'Darko',
+                    lastName: 'Jovanovic',
+                    ready: false,
+                },
+                {
+                    id: 456,
+                    firstName: 'Filip',
+                    lastName: 'Markovic',
+                    ready: true,
+                },
+            ]
+        }
+    }
 
     get styles() {
         return {
             container: {
                 display: 'flex',
-                width: '100%',
             }
         }
     }
@@ -17,7 +44,15 @@ class Lobby extends React.Component {
     render() {
         return (
             <div style={{...this.styles.container, ...this.props.style}}>
-                <CreateGame />
+                <List>
+                    {
+                        this.state.users.map((user, i) =>
+                            <ListItem key={user.id}
+                                      primaryText={user.firstName + " " + user.lastName}
+                                      rightIcon={user.ready ? <DoneIcon /> : <HourglassIcon />}/>
+                        )
+                    }
+                </List>
             </div>
         );
     }
