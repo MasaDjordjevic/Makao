0ecaf72723b639af40e1cfaaf8da3c50e8131aba
Plavusa <masadordevic@gmail.com>
2017-02-12 00:18:37 +0100

    gameInitialiser fix

--- app/client/src/components/Gameplay/GameInitialiser.js
+++ app/client/src/components/Gameplay/GameInitialiser.js
@@ -3,6 +3,7 @@ import RaisedButton from 'material-ui/RaisedButton';
 import FriendPicker from '../Lobby/FriendPicker';
 import Lobby from '../Lobby/Lobby';
 import RulesSetter from '../Lobby/Rules';
+import GlobalVariables from '../Gameplay/GlobalVariables';
 
 class GameInitialiser extends React.Component {
     constructor() {
@@ -43,7 +44,6 @@ class GameInitialiser extends React.Component {
             },
             section: {
                 padding: '0 16px 20px',
-                minWidth: 450,
                 backgroundColor: 'white',
                 boxShadow: 'rgba(0, 0, 0, 0.117647) 0px 1px 6px, rgba(0, 0, 0, 0.117647) 0px 1px 4px',
             },
@@ -70,7 +70,7 @@ class GameInitialiser extends React.Component {
             <div style={{...this.styles.container, ...this.props.style}}>
                 <div style={this.styles.playersContainer}>
                     {
-                        false ? inviteFriends : rules
+                        this.state.creatorId === GlobalVariables.userId ? inviteFriends : rules
                     }
                     <div style={this.styles.section}>
                         <h3 style={this.styles.title}>Lobby</h3>
