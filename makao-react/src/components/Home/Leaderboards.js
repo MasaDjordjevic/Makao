9fcc3b1aec8bd867ecd20b8e9f2a9727807578ae
Plavusa <masadordevic@gmail.com>
2017-02-11 09:23:55 +0100

    leaderboard

--- makao-react/src/components/Home/Leaderboards.js
+++ makao-react/src/components/Home/Leaderboards.js
@@ -1,23 +1,80 @@
 import React from 'react';
 import {Tabs, Tab} from 'material-ui/Tabs';
+import PublicIcon from 'material-ui/svg-icons/social/public';
+import GroupIcon from 'material-ui/svg-icons/social/group';
+import {List, ListItem} from 'material-ui/List';
+import DefultTooltip from '../DefaultTooltip/DefaultTooltip';
+import {grey400,teal800}    from 'material-ui/styles/colors';
 
 class Leaderboards extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            friendsLeaderboard: [
+                {firstName: 'Marko', lastname: 'Petrovic'},
+                {firstName: 'Darko', lastname: 'Markovic'},
+                {firstName: 'Igor', lastname: 'Nikolic'},
+                {firstName: 'Petar', lastname: 'Zivic'},
+                {firstName: 'Filip', lastname: 'Stojanovic'},
+            ],
+            globalLeaderboard: [
+                {firstName: 'Milica', lastname: 'Petrovic', friend: false},
+                {firstName: 'Tijana', lastname: 'Markovic', friend: true},
+                {firstName: 'Kristina', lastname: 'Nikolic', friend: false},
+                {firstName: 'Ana', lastname: 'Zivic', friend: false},
+                {firstName: 'Marina', lastname: 'Stojanovic', friend: true},
+            ],
+        }
+    }
+
     get styles() {
         return {
             container: {
-                width: 450,
+                width: 250,
                 marginLeft: 'auto'
             }
         }
+    }
+
+    getFriendAttributes(user){
+        return user.friend ? {
+                style: {fontWeight: 600},
+                rightIcon: <GroupIcon color={grey400}/>
+            } : {};
+    }
+
+    renderList(userArray, floatRight) {
+
+        return (
+            <List>
+                {
+                    userArray.map((user, i) =>
+                        <ListItem key={i}
+                                  primaryText={(i + 1).toString() + ". " + user.firstName + " " + user.lastname}
+                                  {...this.getFriendAttributes(user)} />
+                    )
+                }
+            </List>
+        )
     }
 
     render() {
         return (
             <div style={{...this.styles.container, ...this.props.style}}>
                 <Tabs>
-                    <Tab label="Friends" >
+                    <Tab
+                        icon={
+                            <DefultTooltip tooltip="friends" tooltipPosition="top-center">
+                                <GroupIcon color={teal800}/>
+                            </DefultTooltip>}>
+                        {this.renderList(this.state.friendsLeaderboard)}
                     </Tab>
-                    <Tab label="All players">
+                    <Tab
+                        icon={
+                            <DefultTooltip tooltip="global" tooltipPosition="top-center">
+                                <PublicIcon color={teal800} />
+                            </DefultTooltip>}>
+                        {this.renderList(this.state.globalLeaderboard, true)}
                     </Tab>
                 </Tabs>
             </div>
