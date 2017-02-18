4b6a51e4efa4fae5c74f29fd09aa3584386a95fd
Plavusa <masadordevic@gmail.com>
2017-02-18 12:18:20 +0100

    snackbar added, draw or play move when its not your tourn

--- app/client/src/components/Game/GameSocketWrapper.js
+++ app/client/src/components/Game/GameSocketWrapper.js
@@ -5,6 +5,8 @@ import io from 'socket.io-client';
 import GameActions from '../../actions/GameActions';
 import Card from '../Card/Card';
 import Game from '../Game/Game';
+import Snackbar from 'material-ui/Snackbar';
+
 
 var socket;
 
@@ -18,7 +20,8 @@ class GameSocketWrapper extends React.Component {
             playerOnMove: '',
             myCards: [],
             openStack: [],
-            jackPlayed: false
+            jackPlayed: false,
+            snackbarOpen: false,
         };
 
         this.handleCardClick = this.handleCardClick.bind(this);
@@ -47,6 +50,7 @@ class GameSocketWrapper extends React.Component {
 
     handleNext() {
         if (this.state.playerOnMove !== this.state.me.username) {
+            this.setState({snackbarOpen:true});
             return;
         }
         socket.emit('play:pass');
@@ -66,6 +70,7 @@ class GameSocketWrapper extends React.Component {
 
     handleCardClick(card) {
         if (this.state.playerOnMove !== this.state.me.username) {
+            this.setState({snackbarOpen:true});
             return;
         }
         this.handleMovePlayed(this.state.me.username, _.find(this.state.myCards, card));
@@ -102,6 +107,7 @@ class GameSocketWrapper extends React.Component {
 
     handleDrawClick() {
         if (this.state.playerOnMove !== this.state.me.username) {
+            this.setState({snackbarOpen: true});
             return;
         }
         socket.emit('play:draw', 1);
@@ -173,6 +179,12 @@ class GameSocketWrapper extends React.Component {
         }
     }
 
+    handleRequestClose = () => {
+        this.setState({
+            snackbarOpen: false,
+        });
+    };
+
     render() {
         const players = this.state.players.slice();
         const playersWithoutUser = _.remove(players, (p) => p.username !== this.state.me.username);
@@ -191,6 +203,12 @@ class GameSocketWrapper extends React.Component {
                       onCardClick={this.handleCardClick}
                       onJackSignPicked={this.handleJackSignPicked}
                       onNext={this.handleNext} />
+                <Snackbar
+                    open={this.state.snackbarOpen}
+                    message="Not your tourn"
+                    autoHideDuration={4000}
+                    onRequestClose={this.handleRequestClose}
+                />
             </div>
         );
     }
