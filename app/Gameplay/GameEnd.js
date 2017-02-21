80f26f6d899255b41c2bef0f789c86b2b0354411
jajac13 <milosjajac13@gmail.com>
2017-02-21 15:56:02 +0100

    saving game to mongodb

--- app/Gameplay/GameEnd.js
+++ app/Gameplay/GameEnd.js
@@ -1,8 +1,30 @@
+import Game from '../models/Game';
+
 let exp = {};
 
 exp.handleGameEnd = (game) => {
-  console.log('game end. Duration: ' + game.duration);
+    console.log('game end. Duration: ' + game.duration);
+    let gameData = {};
+    let players = [], kicked = [];
+    Object.keys(game.players).forEach((username) => {
+        if (game.players[username].kicked) {
+            kicked.push(username);
+        } else {
+            players.push(username);
+        }
+    });
+    gameData.rules = game.rules;
+    gameData.date = game.end;
+    gameData.duration = game.duration;
+    gameData.players = players;
+    gameData.kicked = kicked;
+    gameData.handNum = game.scores.length;
+    gameData.scores = game.scores;
 
+    let newGame = new Game(gameData);
+    newGame.save((err) => {
+        console.log(err);
+    });
 };
 
 module.exports = exp;
